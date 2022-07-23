a01d4ae70ecef06a912a1fa2116bf97b6559c03c
nblaaa <nblaaa@mail.ru>
2022-07-23 19:58:36 +0700

    Make main function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 
-using namespace std;
+#include "multithreadsubstringseeker.h"
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int main(int argc, char** argv) {
+    if (argc < 3 || argc > 4) {
+        throw std::runtime_error("Wrong arguments count!");
+    }
+
+    char anyChar = '?';
+    if (argc == 4) {
+        anyChar = argv[3][0];
+    }
+
+    MultithreadSubstringSeeker seeker;
+    std::vector<SearchResult> result = seeker.seek(argv[1], argv[2], anyChar);
+    
+    std::cout << result.size() << std::endl;
+    for (auto searchResultItem : result) {
+        std::cout << searchResultItem.lineIndex << " " << searchResultItem.startCharIndex << " " << searchResultItem.substring << std::endl;
+    }
+
     return 0;
 }
